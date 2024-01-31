5970a2399ada15403bed47f9b4a03da39995363c
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-01-31 19:46:02 +0530

    Added code to print1ToN using recursion

---
+++ Recursion/print1ToN.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void print1ToN(int n) {
+    if (n == 0)
+        return
+
+    print1ToN(n-1); 
+    cout << n << " ";
+}
+
+int main() 
+{
+    int n; 
+    cout << "Enter n: ";
+    cin >> n; 
+
+    print1ToN(n);
+
+    return 0; 
+}

b2f3b68155b3b472402cc4abde6ba0aa0d802274
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-11 13:09:24 +0530

    Print Permutations of Array/string I

---
+++ Recursion/printPermutations.cpp
@@ -0,0 +1,48 @@
+/**
+ *  Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ *  Example 1:
+ *      Input: nums = [1,2,3] 
+ *      Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *  Problem link: https://leetcode.com/problems/permutations/description/
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std; 
+
+void printVector(vector<int> &v) {
+    
+    for (int i = 0; i < v.size(); i++) 
+        cout << v[i] << " ";
+    cout << endl;
+}
+
+
+void solve(int size, int arr[], vector<int> &ds, unordered_map<int, int> &freq) {
+    //Base case
+    if (ds.size() == size) {
+        printVector(ds);
+        return;
+    }
+    for (int i = 0; i < size; i++) {
+        if (!freq[arr[i]]) {
+            freq[arr[i]] = 1;
+            ds.push_back(arr[i]);
+            solve(size, arr, ds, freq);
+            ds.pop_back();
+            freq[arr[i]] = 0;
+        }
+    }
+}
+
+
+int main() 
+{
+    int arr[]={1, 2, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> ds;
+    unordered_map<int, int> freq;
+    solve(n, arr, ds, freq);
+    return 0; 
+}

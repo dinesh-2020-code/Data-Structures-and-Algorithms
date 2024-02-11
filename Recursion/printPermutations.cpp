683cbc704bcf24fc7c23bf1d5e7db453fea9c30e
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-11 13:35:37 +0530

    Print Permutations of Array/string II

--- Recursion/printPermutations.cpp
+++ Recursion/printPermutations.cpp
@@ -4,6 +4,9 @@
  *      Input: nums = [1,2,3] 
  *      Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  *  Problem link: https://leetcode.com/problems/permutations/description/
+ * 
+ *      Time complexity: n! * n
+ *      Space Complexity: O(n) for map (solve()) and o(1) for solve2()
 */
 
 #include <iostream>
@@ -36,13 +39,32 @@ void solve(int size, int arr[], vector<int> &ds, unordered_map<int, int> &freq) 
     }
 }
 
+// Approach 2 Space efficient uses swapping technique to generate permutations
+void solve2(int ind, int size, int arr[]) {
+    if (ind == size) {
 
-int main() 
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    //Try all possible way for an index ind
+    for (int i = ind; i < size; i++) {
+        swap(arr[i], arr[ind]);
+        solve2(ind + 1, size, arr);
+        swap(arr[i], arr[ind]);
+    }
+}
+
+int main()
 {
     int arr[]={1, 2, 3};
     int n = sizeof(arr)/sizeof(arr[0]);
     vector<int> ds;
     unordered_map<int, int> freq;
-    solve(n, arr, ds, freq);
+    // solve(n, arr, ds, freq);
+    solve2(0, n, arr);
     return 0; 
 }

f930bd21e216cf98464908eb950d7d5dd3faae61
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-15 23:31:34 +0530

    DP1- Fibonacci sequence - Tabluation

--- Dynamic Programming/DP1_fibonacci.cpp
+++ Dynamic Programming/DP1_fibonacci.cpp
@@ -2,7 +2,7 @@
  * Dynamic Programming: 
  *      Two ways.   
  *          1. Memoization: Top Down -> tend to store the value of sub-problems
- *       2. Tabulation: Bottom up approach
+ *       2. Tabulation: Bottom up approach -> Base case to the required ans
  *         Fibonacci Series: 
  *              recurrence relation: f(n) = f(n - 1) + f(n-2)
 */
@@ -20,11 +20,25 @@ int fib(int n, int dp[]) {
     
     dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
 }
+
+int fib_tab(int n) {
+    int dp[n + 1];
+    dp[0] = 0; dp[1] = 1; // write base case in the table
+    
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
 int main() 
 {
     int number; 
     cin >> number;
     int dp[number + 1];
     memset(dp, -1, sizeof(dp));
+    cout << fib(number, dp); 
     return 0;
 }
+
+// Time: O(n)
+// Space : O(n)

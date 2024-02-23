fee465d9f4a9ffeb24ed2fb7c57f3963047cd4fe
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-23 09:47:39 +0530

    DP13 Cherry PickUp II Tabulation

--- Dynamic Programming/DP13_CherryPickupII.cpp
+++ Dynamic Programming/DP13_CherryPickupII.cpp
@@ -53,12 +53,56 @@ int cherryPickup(int row, int col1, int col2, int m, int n, vector<vector<int>> 
     return maxi; 
 }
 
+void cherryPickupTab(int r, int c,
+                vector<vector<int> > &grid) {
+    
+    vector<vector<vector<int>>> dp (r, vector<vector<int>> (c, vector<int> (c, 0)));
+    //fill base case in table
+    for (int col1 = 0; col1 < c; col1++) {
+        for (int col2 = 0; col2 < c; col2++) {
+            if (col1 == col2) {
+                dp[r-1][col1][col2] = grid[r-1][col1]; 
+            }
+            else {
+                dp[r-1][col1][col2] = grid[r-1][col1] + grid[r-1][col2]; 
+            }
+        }
+    }
+    for (int row = r - 2; row >= 0; row--) {
+        for (int col1 = 0; col1 < c; col1++) {
+            for (int col2 = 0; col2 < c; col2++) {
+                int maxi = -1e9;
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        int value = 0; 
+                        //if both robots ended at same cell
+                        if (col1 == col2)
+                            value = grid[row][col1];
+                        else 
+                            value = grid[row][col1] + grid[row][col2];
+
+                        if (col1 + i >= 0 && col1 + i < c && col2 + j >= 0 && col2 + j < c)
+                            value += dp[row + 1][col1 + i][col2 + j];
+                        else 
+                            value += -1e9;
+                        
+                        maxi = max(maxi,  value); 
+                    }
+                }
+                dp[row][col1][col2] = maxi; 
+            }
+        }
+    }
+    cout << dp[0][0][c-1] << "\n"; 
+
+}
 int main() {
     vector<vector<int>> grid = {{3,1,1},{2,5,1},{1,5,5},{2,1,1}};
 
     int m = grid.size(); //rows
     int n = grid[0].size(); //cols
     cout << cherryPickup(0, 0, n - 1, m, n,  grid) << "\n"; 
+    cherryPickupTab(m, n, grid); 
     return 0; 
 }
 
@@ -68,4 +112,12 @@ int main() {
  *      Time: O(3 ^ n * 3 ^ n); 
  *      Aux space: O(n) stacks pace
  * Time
+ *  Memoization: O(rows * cols * cols)
+ *  Aux space: O(n) + O(m * n * n)
+ * 
+ *  Tabulation: 
+ *  Time: O(rows * cols * cols) 
+ *  Aux space: O (m * n * n)
+ * 
+ * 
 */

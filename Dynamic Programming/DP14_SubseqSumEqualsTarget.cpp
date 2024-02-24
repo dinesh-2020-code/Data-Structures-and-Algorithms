d0d37b1d8e6ac7bed4aa858a8ce7715dd6891390
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-24 17:10:14 +0530

    DP14: Subset sum equals K Tabulation

--- Dynamic Programming/DP14_SubseqSumEqualsTarget.cpp
+++ Dynamic Programming/DP14_SubseqSumEqualsTarget.cpp
@@ -58,6 +58,38 @@ int subsetSumEqualsK(int ind, int target, vector<int> &arr, vector<vector<int>> 
 }
 
 
+void subsetSumEqualsKTab(int n, int k, vector<int> &arr) {
+
+    vector<vector<int> > dp (n, vector<int> (k + 1, 0));
+
+    // Fill DP table with base cases
+    // case 1: IF target IS 0: index ranges as  0 <= ind < n
+    // case 2: IF ind IS 0 => if arr[ind] is also 0, then return true;
+
+    // case 1:
+    for (int ind = 0; ind < n; ind++) {
+        dp[ind][0] = 1; 
+    }
+
+    // case 2: 
+    dp[0][arr[0]] = 1; // dp[0][1] means that till index 0 and sum is 1 (acc to input {1, 2, 3, 4})
+
+    for (int ind = 1; ind < n; ind++) {
+        
+        for (int target = 1; target <= k; target++) {  
+            int notTake = dp[ind - 1][target]; 
+            int take = 0; 
+            //take if arr[ind] <= target
+            if (arr[ind] <= target) 
+                take = dp[ind - 1][target - arr[ind]]; 
+
+            dp[ind][target] = take || notTake; 
+        }
+    }
+    cout << dp[n - 1][k] << "\n";
+}
+
+
 int main() {
 
     vector<int> arr = {1, 2, 3, 4}; 
@@ -67,5 +99,21 @@ int main() {
     //we have 2 states index (0 <= ind < n) and target (0 <= target <= k)
     vector<vector<int>> dp(n, vector<int>(k + 1, -1)); 
     cout << subsetSumEqualsK(n - 1, k, arr, dp) << "\n"; 
+    subsetSumEqualsKTab(n, k, arr); 
     return 0; 
 }
+
+/**
+ * Asymptotic Analysis
+ * Recursive code: 
+ *      Time: O(2 ^ n)
+ *      Aux Space: O(n) recursive stack space
+ * 
+ * Memoization: 
+ *      Time: O(n * k)
+ *      Aux Space: O(n) recursive stack space + O (n * k) Dp table
+ * 
+ * Tabulation: 
+ *      Time: O(n * k)
+ *      Aux Space: O(n * k)
+*/

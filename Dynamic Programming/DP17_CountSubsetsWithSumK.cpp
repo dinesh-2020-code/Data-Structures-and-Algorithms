30b3ac2c9707628267756bdccb1bd421ee32cfed
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-27 08:19:50 +0530

    Added Problem Statement for DP17

---
+++ Dynamic Programming/DP17_CountSubsetsWithSumK.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Link: https://www.codingninjas.com/studio/problems/number-of-subsets_3952532
+ * Problem statement
+ * You are given an array 'arr' of size 'n' containing positive integers and a target sum 'k'.
+ * Find the number of ways of selecting the elements from the array such that the sum of chosen 
+ * elements is equal to the target 'k'.
+ * 
+ * Input: 4 5 // N K
+ * array: {1, 4, 4, 5}
+ * 
+ * Output: 3
+ * 
+ * Input: 3 4
+ * array: {0, 1, 3}
+ * 
+ * Output: 2 {0, 1, 3} and {1, 3}
+*/

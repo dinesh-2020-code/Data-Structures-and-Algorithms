92629204a9517cbd51a66d52d88196e658ed676b
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-02-27 08:50:01 +0530

    DP17: Count Subsets with Sum K

--- Dynamic Programming/DP17_CountSubsetsWithSumK.cpp
+++ Dynamic Programming/DP17_CountSubsetsWithSumK.cpp
@@ -15,3 +15,43 @@
  * 
  * Output: 2 {0, 1, 3} and {1, 3}
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int mod = 1000000007; 
+
+int countSubsetsWithSumK(int ind, int curr_sum, vector<int> &arr, vector<vector<int>> &dp) {
+
+	//base cases
+	if (ind == 0) {
+		if (curr_sum == 0 && arr[0] == 0)
+			return 2; // 2 possible case, either take 0 or don't take 0
+		
+		if (curr_sum == 0 || arr[0] == curr_sum)
+			return 1; 
+		else
+			return 0; 
+	}
+	if (dp[ind][curr_sum] != -1)
+		return dp[ind][curr_sum]; 
+
+	int notPick = countSubsetsWithSumK(ind - 1, curr_sum, arr, dp); 
+	int pick    = (arr[ind] <= curr_sum) ? countSubsetsWithSumK(ind - 1, curr_sum - arr[ind], arr, dp) : 0; 
+	return dp[ind][curr_sum] = (notPick + pick ) % mod; 
+}
+
+
+
+int main() {
+
+    vector<int> arr = {0, 0, 3};
+    int k = 3; 
+    int n = arr.size(); 
+    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+    cout << countSubsetsWithSumK(n - 1, k, arr, dp) << "\n";
+
+    return 0;
+}

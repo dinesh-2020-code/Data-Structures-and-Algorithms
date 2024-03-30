de660daaff45af00e0332aaf5c5797d317e1d8cc
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-03-30 23:16:56 +0530

    DP50: Min Cost to Cut the Sticks Tabulated Solution

--- Dynamic Programming/DP50_MinCostToCutTheStick.cpp
+++ Dynamic Programming/DP50_MinCostToCutTheStick.cpp
@@ -43,6 +43,28 @@ int minCost(int i, int j, vector<int> &cuts, vector<vector<int> > &dp) {
 }
 
 
+int minCostTab(vector<int> &cuts, int n) {
+    int c = cuts.size();
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(), 0);
+    sort(cuts.begin(), cuts.end());
+    vector<vector<int> > dp (c + 2, vector<int> (c + 2, 0));
+    for (int i = c; i >= 1; i--) {
+        for (int j = 1; j <= c; j++) {
+            if (i > j)
+                continue;
+            int mini = INT_MAX;
+            for (int ind = i; ind <= j; ind++) {
+                int cost = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                mini = min (mini, cost);
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][c];
+}
+
+
 int memoizedSol(vector<int> &cuts, int n) {
 
     int c = cuts.size();
@@ -50,7 +72,7 @@ int memoizedSol(vector<int> &cuts, int n) {
     cuts.insert(cuts.begin(), 0);
     sort(cuts.begin(), cuts.end());
     vector<vector<int> > dp (c + 1, vector<int> (c + 1, -1));
-    cout << "Min cost is: " << minCost(1, c, cuts, dp) << endl;
+    return minCost(1, c, cuts, dp);
 }
 
 
@@ -58,6 +80,7 @@ int main() {
 
     vector<int> cuts = {1, 3, 4, 5};
     int n = 7;
-    return memoizedSol(cuts, n);
+    cout << "Min cost is: " << memoizedSol(cuts, n) << "\n";
+    cout << "Min cost is: " << minCostTab(cuts, n) << "\n";
 
 }

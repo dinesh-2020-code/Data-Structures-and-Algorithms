e5882639e13404179b13b1b113a401b987b15154
Dinesh Kumar <thedineshkumar2002@gmail.com>
2024-04-07 17:49:57 +0530

    LC678_Valid Parantheses String(Efficient Sol)

--- Dynamic Programming/DP57_LC678_ValidParanthesesString.cpp
+++ Dynamic Programming/DP57_LC678_ValidParanthesesString.cpp
@@ -21,11 +21,12 @@
  * 
  * Input: s = "((*)(*))()*(*)****((*(*)())*()((()**(**)"    Output: True
  * Input: s = "(*()(**(*)())*))())())*)()()*(((*)()))(**()*)**(*"       Output: True
- * Input: s = "(((((()*)(*)*))())())(()())())))((**)))))(()())()"       Outupt: False
+ * Input: s = "(((((()*)(*)*))())())(()())())))((**)))))(()())()"       Output: False
 */
 
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -64,6 +65,7 @@ bool solve(int ind, int count, string &s, vector<vector<int>> &dp)
 
 bool memo(string s)
 {
+    // Time: O(n^2) Aux Space: O(n^2) + O (n)
     int n = s.size();
     vector<vector<int>> dp(n, vector<int>(n, -1));
     return solve(0, 0, s, dp);
@@ -71,6 +73,7 @@ bool memo(string s)
 
 bool checkValidStringTab(string &s)
 {
+    // Time: O(n^2) Aux Space: O(n^2)
     // write base cases in the dp table
     int n = s.size();
     vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
@@ -100,6 +103,51 @@ bool checkValidStringTab(string &s)
     return dp[0][0];
 }
 
+bool twoStacksSol(string &s)
+{
+    // Time: O(n) Aux space: O(2n) for two stacks
+    int n = s.size();
+    stack<int> openBrackets, asterisks;
+
+    for (int i = 0; i < n; i++)
+    {
+        char ch = s[i];
+
+        // if current char is an open bracket, push its index onto the stack
+        if (ch == '(')
+            openBrackets.push(i);
+        else if (ch == '*')
+            asterisks.push(i);
+        else
+        { // if current char is a closing bracket
+            // if opening brackets available, use them to balance the closing bracket
+            if (!openBrackets.empty())
+                openBrackets.pop();
+
+            // if no opening brackets available, use an asterisk to balance the closing bracket
+            else if (!asterisks.empty())
+                asterisks.pop();
+
+            // Neither openbrackets nor asterisks remaining, there is no way ')' can match
+            else
+                return false;
+        }
+    }
+
+    // check if there are remaining open brackets and asterisks that can balance them
+    while (!openBrackets.empty() && !asterisks.empty())
+    {
+        // if an open bracket appears after an asterisk, it cannot be balanced, return false
+        if (openBrackets.top() > asterisks.top())
+            return false;
+
+        asterisks.pop();
+        openBrackets.pop();
+    }
+    // if all open brackets are matched and there are no unmatched open brackets left, return true
+    return openBrackets.empty();
+}
+
 
 int main()
 {
@@ -107,5 +155,6 @@ int main()
     string s = "(((((()*)(*)*))())())(()())())))((**)))))(()())()";
     cout << "isValidString: " << memo(s) << endl;
     cout << "isValidString: " << checkValidStringTab(s) << endl;
+    cout << "isValidString: " << twoStacksSol(s) << endl;
     return 0;
 }
